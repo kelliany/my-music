6af378fbd0ba7f2ef1ddfe5f5e893975873ba8e3
kelliany <965526122@qq.com>
2018-06-11 18:07:11 +0800

    slider down

--- src/components/v-slider/v-slider.vue
+++ src/components/v-slider/v-slider.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="slider" v-show="isShow">
-        
+    <div class="slider"  v-show="isShowSlider" @click.stop="closeSlider">
+        <div class="slider-content" :class="{'move':isShowSlider}"  ></div>
     </div>
 </template>
 <script>
@@ -9,28 +9,50 @@ export default {
     name:'v-slider',
     data() {
         return {
-            isShow: false
+            isShowSlider: false
         }
     },
-    
+    methods:{
+        closeSlider() {
+            this.isShowSlider = false;
+        }
+    },
     mounted() {
         let self = this
         Bus.$on('change', function(get) {
-            self.isShow = get            
+            self.isShowSlider = get            
         })
     }
     
 }
 </script>
 <style lang="less">
- .slider{
+.slider{
+    width: 100%;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    transition: width .5s;
+    .slider-content{
      background-color: #333;
      opacity: 0.8;
      position: absolute; 
      top: 0;
+     width: 0%;
      height: 100%;
-     width: 60%;
+    
      z-index: 2;
- }
+    }
+    .move {
+        width: 60%;
+        transition: width .5s;
+
+    }
+
+}
+
+ 
 </style>
 
